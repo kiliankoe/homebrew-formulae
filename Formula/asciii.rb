90cbdd4e85cb26cd71f1f3a559d009f1cef5c07b
Kilian Koeltzsch <me@kilian.io>
2017-01-09 14:00:10 +0100

    fix asciii formula
    so apparently the tag is not optional?

--- Formula/asciii.rb
+++ Formula/asciii.rb
@@ -1,6 +1,6 @@
 class Asciii < Formula
   homepage "https://github.com/ascii-dresden/asciii"
-  url "https://github.com/ascii-dresden/asciii.git"
+  url "https://github.com/ascii-dresden/asciii.git", :tag => "3.0.0"
 
   def install
     system 'cargo install', '--git', 'https://github.com/ascii-dresden/asciii'
