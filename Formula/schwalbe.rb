a59aee24af1cde968e562f6a95ad566a80a49d79
Kilian Koeltzsch <me@kilian.io>
2017-03-17 21:59:19 +0100

    add description to schwalbe formula

--- Formula/schwalbe.rb
+++ Formula/schwalbe.rb
@@ -1,5 +1,6 @@
 class Schwalbe < Formula
   homepage "https://github.com/kiliankoe/schwalbe"
+  desc "Transpiler from Schwalbe to Swift"
   url "https://github.com/kiliankoe/schwalbe/releases/download/0.3.0/schwalbe"
   sha256 "3fbcebc59bc1418e9a605d44da2ccf30dc699830354b1e49ffc60b4a48424184"
 
