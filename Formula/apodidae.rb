1078291c49ce9b32f6833dae65b51d75c7aae33b
Kilian Koeltzsch <me@kilian.io>
2017-07-30 15:18:23 +0200

    bump apodidae version

--- Formula/apodidae.rb
+++ Formula/apodidae.rb
@@ -1,8 +1,8 @@
 class Apodidae < Formula
   homepage "https://github.com/kiliankoe/apodidae"
   desc "Command line client for packagecatalog.com"
-  url "https://github.com/kiliankoe/apodidae/releases/download/0.1.0/0.1.0.tar.gz"
-  sha256 "0dc8f5c2f0d0392092300296a51191814653465ed6aabcdf9f6cef0e914562f0"
+  url "https://github.com/kiliankoe/apodidae/releases/download/0.2.0/0.2.0.tar.gz"
+  sha256 "f63aad1aedabba11a04251a6c8b19d5019ad14736a069b571f12cca6c88eb5ec"
 
   def install
     bin.install "apo"
