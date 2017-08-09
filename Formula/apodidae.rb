56abba921edeeccbf609f9b99eae1a403d5d09d3
Kilian Koeltzsch <me@kilian.io>
2017-08-09 12:14:17 +0200

    bump apodidae version

--- Formula/apodidae.rb
+++ Formula/apodidae.rb
@@ -1,8 +1,8 @@
 class Apodidae < Formula
   homepage "https://github.com/kiliankoe/apodidae"
   desc "Search for Swift packages from your command line"
-  url "https://github.com/kiliankoe/apodidae/releases/download/0.4.1/apodidae.tar.gz"
-  sha256 "06030c970ac5909a7104868de832026ea85ec92a57dac5b8c4f8bb5f8b747728"
+  url "https://github.com/kiliankoe/apodidae/releases/download/0.5.0/apodidae.tar.gz"
+  sha256 "a7359b3d9a668e62949d8e69c3aee9044beab6e7ffa63510ae927fa5b1006d9f"
 
   def install
     bin.install "swift-catalog"
