70cd9ac12cd2322e496dd8963b40099d13eca2ab
Kilian Koeltzsch <me@kilian.io>
2019-08-17 14:58:53 +0200

    update swiftlibrary

--- Formula/swiftlibrary.rb
+++ Formula/swiftlibrary.rb
@@ -1,8 +1,8 @@
 class Swiftlibrary < Formula
   homepage "https://github.com/kiliankoe/SwiftLibrary"
   desc "Search for Swift packages from your command line"
-  url "https://github.com/kiliankoe/SwiftLibrary/releases/download/0.6.0/swiftlibrary.tar.gz"
-  sha256 "ca418de9f75355a706338056122e838231d8ee64bdc1b2079526c96240b5e78d"
+  url "https://github.com/kiliankoe/SwiftLibrary/releases/download/0.7.0/swiftlibrary.tar.gz"
+  sha256 "dc8946c89bd989c16d7f8977f554cac5f0cd88f2fed42af5ecabfa5a3c199538"
 
   def install
     bin.install "swift-library"
