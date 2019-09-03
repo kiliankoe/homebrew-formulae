a85a61e7b3c5dca3a676248bd7c72ade37b62a0f
Kilian Koeltzsch <me@kilian.io>
2019-09-03 14:56:16 +0200

    bump swiftlibrary to 0.8.0

--- Formula/swiftlibrary.rb
+++ Formula/swiftlibrary.rb
@@ -1,8 +1,8 @@
 class Swiftlibrary < Formula
   homepage "https://github.com/kiliankoe/SwiftLibrary"
   desc "Search for Swift packages from your command line"
-  url "https://github.com/kiliankoe/SwiftLibrary/releases/download/0.7.0/swiftlibrary.tar.gz"
-  sha256 "dc8946c89bd989c16d7f8977f554cac5f0cd88f2fed42af5ecabfa5a3c199538"
+  url "https://github.com/kiliankoe/SwiftLibrary/releases/download/0.8.0/swiftlibrary.tar.gz"
+  sha256 "99ffbd29f08209879767bfcf5d4c622b9aab3d32fc249b952d36d294ead9349b"
 
   def install
     bin.install "swift-library"
